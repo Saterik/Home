db1f747ca6e3965de7c512c484ea6aa83f6ce797
warfa@LAPTOP-BGKII3AT <warfacesdr@mail.ru>
2022-12-28 23:27:48 +0800

    Задача №10

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = number / 10;
+ if(number / 10 < number % 10);
+ Console.WriteLine(number % 10); 
+   
+
+   
